d58414dd6a2694e8eabddd719def6b5efef8eae3
brittanymbui <brittymbui@gmail.com>
2021-07-05 15:01:04 -0700

    Connects DB

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const connectionString = 'mongodb://127.0.0.1:27017/survey'
+
+mongoose
+    .connect(connectionString, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log('Mongodb connected')
+    })
 
 const PORT = 2000;
 
